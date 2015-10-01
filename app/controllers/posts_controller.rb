384c6edf3a674bf755693492ad0f4d89147272ef
Jack Sung <magicsung@gmail.com>
2015-10-01 11:51:31 +0800

    post CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,56 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, :only => [ :show, :edit, :update, :destroy ]
+
   def index
     @posts = Post.all
   end
 
+  def show
+  end
+
   def new
-    
+    @post = Post.new
   end
 
   def create
+    @post = Post.new( post_params )
     
-  end
-
-  def show
+    if @post.save
+      flash[:notice] = "Post was successfully created!"
+      redirect_to :action => :index  
+    else
+      flash.now[:alert] = "Please enter something!"
+      render "new"
+    end
     
   end
 
   def edit
-    
   end
 
   def update
-    
+    @post.update( post_params )
+
+    redirect_to :action => :index
   end
 
   def destroy
-    
+    @post.destroy
+
+    redirect_to :action => :index
   end
+
 
 
   private
 
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find( params[:id] )
+  end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
 
+  validates_presence_of :title, :content
+
 end
